009580ba956b4e30ef6246d805ed3fee88612647
Cassy-16 <cassidynangmo@gmail.com>
2022-06-11 08:22:38 +0200

    I have coded City entity and Factory.

---
+++ src/test/java/za/ac/cput/school_management/factory/lookup/CityFactoryTest.java
@@ -0,0 +1,16 @@
+package za.ac.cput.school_management.factory.lookup;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.school_management.domain.lookup.City;
+import za.ac.cput.school_management.domain.lookup.Country;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityFactoryTest {
+        City city;
+    @Test
+    void getCity() {
+       city = CityFactory.getCity("12344","Yawunde",new Country());
+        assertNotNull(city);
+    }
+}
