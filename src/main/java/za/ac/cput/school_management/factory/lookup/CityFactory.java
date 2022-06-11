6ea0ddee66b9b75193a1f66924d6affa0d1a0210
Cassy-16 <cassidynangmo@gmail.com>
2022-06-11 08:17:36 +0200

    I have coded City entity and Factory.

--- src/main/java/za/ac/cput/school_management/factory/lookup/CityFactory.java
+++ src/main/java/za/ac/cput/school_management/factory/lookup/CityFactory.java
@@ -1,4 +1,12 @@
 package za.ac.cput.school_management.factory.lookup;
 
+import za.ac.cput.school_management.domain.lookup.City;
+import za.ac.cput.school_management.domain.lookup.Country;
+
+import java.util.UUID;
+
 public class CityFactory {
+    public static City getCity(String id,String name, Country country){
+        return new City.Builder(id).buildName(name).build();
+    }
 }
