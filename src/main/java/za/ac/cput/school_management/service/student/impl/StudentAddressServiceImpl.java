cb179de55be044b60830789e6841cc0d03e8b6dd
adecel <rustymabiala150@gmail.com>
2022-06-14 03:20:38 +0200

    pushing all about Repository and service (the IRepository and the IService are complite now)  the controller is also ready.

---
+++ src/main/java/za/ac/cput/school_management/service/student/impl/StudentAddressServiceImpl.java
@@ -0,0 +1,42 @@
+package za.ac.cput.school_management.service.student.impl;
+
+import za.ac.cput.school_management.repository.student.StudentAddressRepository;
+import za.ac.cput.school_management.repository.student.impl.StudentAddressRepositoryImpl;
+import za.ac.cput.school_management.service.student.StudentAddressService;
+
+import java.util.List;
+import java.util.Optional;
+
+//public class StudentAddressServiceImpl implements StudentAddressService {
+//
+//        private final StudentAddressRepository repository;
+//    private static StudentAddressService SERVICE;
+//
+//    private StudentAddressServiceImpl(){
+//        this.repository = StudentAddressRepositoryImpl.studentAddressRepository();
+//    }
+//    public static StudentAddressService getService(){
+//        if (SERVICE == null)
+//            SERVICE = new StudentAddressServiceImpl();
+//        return SERVICE;
+//
+//    @Override
+//    public StudentAddressService save(StudentAddressService studentAddressService) {
+//            return this.repository.save(studentAddressService);
+//    }
+//
+//    @Override
+//    public Optional<StudentAddressService> read(String s) {
+//        return Optional.empty();
+//    }
+//
+//    @Override
+//    public void delete(StudentAddressService studentAddressService) {
+//
+//    }
+//
+//    @Override
+//    public List<StudentAddressService> findByStudentId(String studentId) {
+//        return null;
+//    }
+//}
