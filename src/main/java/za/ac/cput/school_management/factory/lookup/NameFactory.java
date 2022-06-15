62a79d8407103a41c3a894aaf462909ba56ce345
DieuciND <220169136@mycput.ac.za>
2022-06-15 22:00:47 +0200

    Pushing my classes Name ,NameRepo, NameFactory

--- src/main/java/za/ac/cput/school_management/factory/lookup/NameFactory.java
+++ src/main/java/za/ac/cput/school_management/factory/lookup/NameFactory.java
@@ -1,4 +1,16 @@
 package za.ac.cput.school_management.factory.lookup;
 
+import org.apache.commons.lang3.StringUtils;
+import za.ac.cput.school_management.domain.lookup.Name;
+
 public class NameFactory {
+    public static Name build (String firstName, String middleName, String lastName) {
+        if (firstName==null || firstName.isEmpty() ) {
+            throw new IllegalArgumentException("First Name cannot be null or empty");
+        }
+        middleName = StringUtils.isEmpty(middleName) ? " " : middleName;
+        if (lastName==null ||lastName.isEmpty() )
+            throw new IllegalArgumentException("Last Name cannot be null or empty");
+        return new Name.Builder().firstName(firstName).middleName(middleName).lastName(lastName).build();
+    }
 }
